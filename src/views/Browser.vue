7b06587415586e451f4f37fb9e8e0e62d6191601
bfbd <bfbd@sina.com>
2019-06-25 09:19:06 +0800

    demo lines chart

--- src/views/Browser.vue
+++ src/views/Browser.vue
@@ -1,45 +1,79 @@
 <template>
   <div id='app'>
-    <v-chart class='my-chart' :options='bar'/>
+    <v-chart class='v-chart' :options='options' />
   </div>
 </template>
+
 <script>
-import ECharts from 'vue-echarts/components/ECharts';
-import 'echarts/lib/chart/bar';
-export default {
-  name: 'App',
-  components: {
-    'v-chart': ECharts
-  },
-  data() {
-    return {
-      bar: {
-        title: {
-          text: 'ECharts 入门示例'
-        },
-        tooltip: {},
-        legend: {
-          data: ['销量']
-        },
-        xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-        },
-        yAxis: {},
-        series: [
-          {
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20],
-          }
-        ]
-      }
-    };
-  }
-};
+  import ECharts from 'vue-echarts';
+  import 'echarts/lib/chart/line';
+  export default {
+    name: 'App',
+    components: {
+      'v-chart': ECharts
+    },
+    data() {
+      return {
+        options: {
+          title: {
+            text: '温度曲线'
+          },
+          legend: {
+            data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [{
+              name: '邮件营销',
+              type: 'line',
+              stack: '总量',
+              data: [120, 132, 101, 134, 90, 230, 210]
+            },
+            {
+              name: '联盟广告',
+              type: 'line',
+              stack: '总量',
+              data: [220, 182, 191, 234, 290, 330, 310]
+            },
+            {
+              name: '视频广告',
+              type: 'line',
+              stack: '总量',
+              data: [150, 232, 201, 154, 190, 330, 410]
+            },
+            {
+              name: '直接访问',
+              type: 'line',
+              stack: '总量',
+              data: [320, 332, 301, 334, 390, 330, 320]
+            },
+            {
+              name: '搜索引擎',
+              type: 'line',
+              stack: '总量',
+              data: [820, 932, 901, 934, 1290, 1330, 1320]
+            }
+          ]
+        }
+      };
+    }
+  };
 </script>
 <style>
-.my-chart {
-  width: 800px;
-  height: 500px;
-}
+  .v-chart {
+    width: 800px;
+    height: 500px;
+  }
 </style>
